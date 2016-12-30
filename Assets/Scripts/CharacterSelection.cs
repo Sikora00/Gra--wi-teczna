a1b600380676891d8baf0f9d04b5f8642d511624
Sikora00 <mikon0096@gmail.com>
2016-12-30 02:10:17 +0100

    3
    Wybór bohatera ale źle w sieci

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour {
+
+    private GameObject[] characterList;
+    private int index;
+
+	// Use this for initialization
+	void Start ()
+    {
+        index = PlayerPrefs.GetInt("CharacterSelected");
+        characterList = new GameObject[transform.childCount];
+        
+        for (int i = 0; i<transform.childCount; i++)
+            characterList[i] = transform.GetChild(i).gameObject;
+
+        foreach (GameObject go in characterList)
+            go.SetActive(false);
+
+        if (characterList[index])
+            characterList[index].SetActive(true);
+        characterList[0].SetActive(true);
+
+    }
+
+    public void ToggleLeft()
+    {
+        characterList[index].SetActive(false);
+        index--;
+        if (index < 1)
+            index = characterList.Length - 1;
+
+        characterList[index].SetActive(true);
+    }
+
+    public void ToggleRight()
+    {
+        characterList[index].SetActive(false);
+        index++;
+        if (index > characterList.Length-1)
+            index = 1;
+
+        characterList[index].SetActive(true);
+    }
+
+    public void ConfirmButton()
+    {
+        PlayerPrefs.SetInt("CharacterSelected", index);
+        SceneManager.LoadScene("Game");
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class Movement : MonoBehaviour
+public class Movement : NetworkBehaviour
 {
     // Use this for initialization
     public float Speed = 10;
     public float Force = 300;
     Rigidbody2D rigidbody;
-    private Animator anim;
-    private SpriteRenderer mySpriteRenderer;
     private Transform groundCheck;
     [SerializeField]
     private LayerMask whatIsGround; // A mask determining what is ground to the character
@@ -21,45 +20,42 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
         groundCheck = transform.Find("GroundCheck");
     }
 
     void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
-        if (grounded) anim.SetBool("grounded", true);
-        else anim.SetBool("grounded", false);
     }
     // Update is called once per frame
     void Update()
     {
-
+        if (!isLocalPlayer) return;
 
         if (Input.GetKeyDown(jump) && grounded)
         {
             rigidbody.velocity = new Vector2(0, 0);
             rigidbody.AddForce(new Vector2(0, Force));
-            anim.SetBool("grounded", false);
         }
 
         if (Input.GetKey(right))
         {
             rigidbody.velocity = new Vector2(Speed, rigidbody.velocity.y);
-            if (grounded) anim.SetFloat("velocity", 10);
             transform.localScale = new Vector3(3, 3, 3);
         }
         else if (Input.GetKey(left))
         {
             rigidbody.velocity = new Vector2(-Speed, rigidbody.velocity.y);
-            if (grounded) anim.SetFloat("velocity", 10);
             transform.localScale = new Vector3(-3, 3, 3);
         }
         else
         {
-            anim.SetFloat("velocity", 0);
             rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.LoadLevel(0);
         }
     }
 }
